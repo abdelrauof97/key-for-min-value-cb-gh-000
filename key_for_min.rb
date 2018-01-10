b4914a82f5cc2f60a4c93aeb180d5d421fc9ae41
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-10 23:18:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,9 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
-  
+  lowest_value = nil 
+  lowest_key = nil 
+  name_hash.each { |key,value|
+    if lowest_value == nil || value < lowest_value
+  }
 end
 puts key_for_min_value(ikea)
