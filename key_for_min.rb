b008f503b113cf54298cb1a8ef23335418db31e6
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-10 23:19:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,10 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   name_hash.each { |key,value|
     if lowest_value == nil || value < lowest_value
+      lowest_value
+      lowest_key
+    end 
   }
+  lowest_key
 end
 puts key_for_min_value(ikea)
