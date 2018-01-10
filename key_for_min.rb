31863840a95e56270a13424d19d8f1c1b76bb3b0
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-10 23:21:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   name_hash.each { |key,value|
     if lowest_value == nil || value < lowest_value
-      lowest_value
-      lowest_key
-    end 
+      lowest_value = value
+      lowest_key = key
+    end  
   }
   lowest_key
 end
